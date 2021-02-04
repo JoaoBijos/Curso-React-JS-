40d75150ec6fb39ece2e85a527f5fc456057c558
JoaoBijos <joaopedrobijos96@gmail.com>
2021-02-04 10:29:36 -0300

    Class component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 // sempre que quiser utilizar o React, devemos importar ele para o arquivo
-import React from "react";
+import React,{ Component } from "react";
 
+// criando um componente que é tambem uma class que provem de Component 
+// render() é o que faz a tela ser renderizada e o return é o que retorna para a tela o que será renderizado 
+// class já tem acesso ao props não precisando assim passar os parametros
+// para acessar props deve-se escrever {this.props + o nome da tag}
+class Equipe extends Component{
+    render(){
+      return(
+        <div>
+         <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+         <hr/>
+        </div>
+      )
+    }
+}
 
- // esse componente recebe os parametros passados por App() 
- // e chama outro passando também os mesmos parametros para ele
- const Equipe = (props) => {
-  return (
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-      <Social facebook={props.facebook} linkedin={props.linkedin} youtube={props.youtube}/>
-      <hr/>
-    </div>
-  );
-};
-
-
-// recebe os parametros de Equipe() e executa os comandos com eles 
-const Sobre = (props) => {
-  return (
-    <div>
-      <h2>Nome: {props.nome}</h2>
-      <h3>Cargo: {props.cargo}</h3>
-      <h3>Idade: {props.idade} anos</h3>
-    </div>
-  );
-};
-
-const Social = props => {
-  return(
-    <div>
-      <a href={props.facebook}> Facebook </a>
-      <a href={props.linkedin}> LinkedIn </a>
-      <a href={props.youtube}> YouTube </a>
-    </div>
-  )
+class Sobre extends Component{
+  render(){
+    return(
+      <div>
+          <h2>Nome: {this.props.nome}</h2>
+          <h3>Cargo: {this.props.cargo}</h3>
+          <h3>Idade: {this.props.idade} anos</h3>
+        </div>
+    )
+  }
 }
 
 // aqui é chamado o componente Bemvindo, e também é passado para ele parametros
@@ -41,9 +33,8 @@ function App() {
   return (
     <div>
       <h1>Conheça nossa equipe:</h1>
-      <Equipe nome="João" cargo="Progamador" idade="29" facebook="https://pt-br.facebook.com/" linkedin="https://br.linkedin.com/" youtube="https://www.youtube.com/?gl=BR&tab=r1"/>
-      <Equipe nome="Maria" cargo="Analista" idade="32" facebook="https://pt-br.facebook.com/" linkedin="https://br.linkedin.com/" youtube="https://www.youtube.com/?gl=BR&tab=r1"/>
-      <Equipe nome="Felipe" cargo="Designer" idade="18" facebook="https://pt-br.facebook.com/" linkedin="https://br.linkedin.com/" youtube="https://www.youtube.com/?gl=BR&tab=r1"/>
+      <Equipe nome='Matheus' cargo='Progamador' idade='24'/>
+      <Equipe nome='Lucas' cargo='Analista' idade='30 '/>
     </div>
   );
 }
