8af1c85d3a93c18e0b429252c1d10f6ae13588f8
JoaoBijos <joaopedrobijos96@gmail.com>
2021-02-04 14:34:24 -0300

    States

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-// sempre que quiser utilizar o React, devemos importar ele para o arquivo
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
-// criando um componente que é tambem uma class que provem de Component 
-// render() é o que faz a tela ser renderizada e o return é o que retorna para a tela o que será renderizado 
-// class já tem acesso ao props não precisando assim passar os parametros
-// para acessar props deve-se escrever {this.props + o nome da tag}
-class Equipe extends Component{
-    render(){
-      return(
-        <div>
-         <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
-         <hr/>
-        </div>
-      )
+// as props são propiedades estáticas
+// já as states são propiedades que conseguimos alterar os valores
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "Matheus",
+      contador: 0,
+    };
+
+    // damos o bind para a função poder ser acessada com maior facilidade
+    this.aumentar = this.aumentar.bind(this)
+    this.diminuir = this.diminuir.bind(this)
+  }
+
+  // para alterar o state usamo o this.setState()
+  aumentar(){
+    let state = this.state
+    state.contador += 1
+    state.nome = 'Jorge'
+    this.setState(state)
+  }
+
+  diminuir(){
+    let state = this.state
+    if(state.contador === 0){
+      alert('Opa! De 0 não passa!')
+      return
     }
-}
+    state.contador -= 1
+    state.nome = 'Pedro'
+    this.setState(state)
+  }
 
-class Sobre extends Component{
-  render(){
-    return(
+  render() {
+    return (
       <div>
-          <h2>Nome: {this.props.nome}</h2>
-          <h3>Cargo: {this.props.cargo}</h3>
-          <h3>Idade: {this.props.idade} anos</h3>
-        </div>
-    )
+        <h1>Contador</h1>
+        <h2>{this.state.nome}</h2>
+        <h3>
+          <button onClick={this.diminuir}> - </button>
+          {this.state.contador}
+          <button onClick={this.aumentar}> + </button>
+        </h3>
+      </div>
+    ); 
   }
 }
 
-// aqui é chamado o componente Bemvindo, e também é passado para ele parametros
-function App() {
-  return (
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe nome='Matheus' cargo='Progamador' idade='24'/>
-      <Equipe nome='Lucas' cargo='Analista' idade='30 '/>
-    </div>
-  );
-}
-
-// usando o export poderemos utilizar as funcionalidades do arquivo em outros arquivos quantas vezes for necessário
 export default App;
