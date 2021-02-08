2fb5572de93dda888b6ab9eaea5d72efb563ed70
JoaoBijos <joaopedrobijos96@gmail.com>
2021-02-08 15:16:17 -0300

    Praticando formulários

--- src/App.js
+++ src/App.js
@@ -4,34 +4,55 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      senha: '',
-      sexo: ''
+      nome: "",
+      email: "",
+      senha: "",
     };
 
+    this.cadastrar = this.cadastrar.bind(this)
+  }
+
+  cadastrar(event){
+    const {nome,email,senha} = this.state
+
+    if(nome != '' && email != '' && senha != ''){
+      alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`)
+    } else{
+      alert('Preencha todos os campos antes de cadastrar')
+    }
+
+    // pegamos o evento que chamamos esta função para que a página não atualize
+    event.preventDefault()
   }
 
   render() {
     return (
       <div>
-        <h2>Cadastro</h2>
-        Email :
-        <input type='email' name='email' value={this.state.email}
-               onChange={(e) => this.setState({email: e.target.value})}/><br/>
-        Senha :
-        <input type='password' name='senha' value={this.state.senha}
-               onChange={(e) => this.setState({senha: e.target.value})}/><br/>
-        Sexo:
-        <select name='sexo' value={this.state.sexo} onChange={(e) => this.setState({sexo: e.target.value})}>
-          <option value='masculino'>Maculino</option>
-          <option value='feminino'>Feminino</option>
-        </select>
-
-        <div>
-          <h3>{this.state.email}</h3>
-          <h3>{this.state.senha}</h3>
-          <h3>{this.state.sexo}</h3>
-        </div>
+        <h1>Novo usuário</h1>
+        <form onSubmit={this.cadastrar}>
+          <label>Nome :</label>
+          <input
+            type="text"
+            value={this.state.nome}
+            onChange={(e) => this.setState({ nome: e.target.value })}
+          />
+          <br/>
+          <label>Email :</label>
+          <input
+            type="email"
+            value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })}
+          />
+          <br/>
+          <label>Senha :</label>
+          <input
+            type="password"
+            value={this.state.senha}
+            onChange={(e) => this.setState({ senha: e.target.value })}
+          />
+          <br/>
+          <button type='submit'>Cadastrar</button>
+        </form>
       </div>
     );
   }
