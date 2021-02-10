6c44a673e947e9971e639bd84a701c36e57d0c18
JoaoBijos <joaopedrobijos96@gmail.com>
2021-02-10 11:13:11 -0300

    Cronômetro

--- src/App.js
+++ src/App.js
@@ -1,56 +1,63 @@
 import React, { Component } from "react";
+import imgCronometro from './assets/cronometro.png'
 import './style.css'
-import biscoito from './assets/biscoito.png';
 
 class App extends Component {
 
   constructor(props){
     super(props)
     this.state = {
-      textoFrase: ''
+      numero: 0,
+      botao: 'GO'
     }
-
-    this.quebraBiscoito = this.quebraBiscoito.bind(this)
-
-    this.frases = ['O Aspira só sabe aspirar, por isso, cuidado ao espirrar',
-    'Thiago é um bom companheiro, ninguém pode negar, só não deixa a bebida ele pegar',
-    'Pedrão é o pai de todos não tem como negar, mas se mal dele você falar, como Thiago você vai ter que lidar',
-    'A Daily é o momento mais divertido do dia, melhor ainda é quando o Thiago suas piadas fazia',
-    'Seu sucesso só depende de você! Pode começar a chorar !!',
-    'Pedra mole água dura, tanto bate até que batata',
-    'O importante é o que importa',
-    'A vida é só duas, coma churrasco',
-    'Sem chifre, sem defesa']
+    this.timer = null
+    this.go = this.go.bind(this)
+    this.reset = this.reset.bind(this)
   }
 
-  quebraBiscoito(){
+  go(){
     let state = this.state;
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
-    state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "'
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = 'START';
+    }else{
+      this.timer = setInterval(()=>{
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      },100);
+      state.botao = 'STOP';
+    }
+
     this.setState(state);
-}
-  
+  }
+
+  reset(){
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    let state = this.state;
+    state.numero = 0;
+    state.botao = 'START';
+    this.setState(state);
+  }
+
   render() {
     return (
       <div className='container'>
-        <img src={biscoito} className='img'/>
-        <Botao nome='Abrir biscoito' acaoBtn={this.quebraBiscoito}/>
-        <h3 className='textofrase'>{this.state.textoFrase}</h3>
+        <img src={imgCronometro} className='img'/>
+        <a className='timer'>{this.state.numero.toFixed(1)}</a>
+        <div className='areaBtn'>
+          <a className='botao' onClick={this.go}>{this.state.botao}</a>
+          <a className='botao' onClick={this.reset}>RESET</a>
+        </div>
       </div>
     );
   }
 }
-
-class Botao extends Component{
-  render(){
-    return(
-      <div>
-         <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
-      </div>
-    )
-  }
-}
-
-
 
 export default App;
