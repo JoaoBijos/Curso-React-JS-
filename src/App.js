b5274c4065aa9fac21db464f9650cec2a49efc3f
JoaoBijos <joaopedrobijos96@gmail.com>
2021-02-10 10:35:06 -0300

    Biscoito da Sorte

--- src/App.js
+++ src/App.js
@@ -1,61 +1,56 @@
 import React, { Component } from "react";
+import './style.css'
+import biscoito from './assets/biscoito.png';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+
+  constructor(props){
+    super(props)
     this.state = {
-      nome: "",
-      email: "",
-      senha: "",
-    };
-
-    this.cadastrar = this.cadastrar.bind(this)
-  }
-
-  cadastrar(event){
-    const {nome,email,senha} = this.state
-
-    if(nome != '' && email != '' && senha != ''){
-      alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`)
-    } else{
-      alert('Preencha todos os campos antes de cadastrar')
+      textoFrase: ''
     }
 
-    // pegamos o evento que chamamos esta função para que a página não atualize
-    event.preventDefault()
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+
+    this.frases = ['O Aspira só sabe aspirar, por isso, cuidado ao espirrar',
+    'Thiago é um bom companheiro, ninguém pode negar, só não deixa a bebida ele pegar',
+    'Pedrão é o pai de todos não tem como negar, mas se mal dele você falar, como Thiago você vai ter que lidar',
+    'A Daily é o momento mais divertido do dia, melhor ainda é quando o Thiago suas piadas fazia',
+    'Seu sucesso só depende de você! Pode começar a chorar !!',
+    'Pedra mole água dura, tanto bate até que batata',
+    'O importante é o que importa',
+    'A vida é só duas, coma churrasco',
+    'Sem chifre, sem defesa']
   }
 
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "'
+    this.setState(state);
+}
+  
   render() {
     return (
-      <div>
-        <h1>Novo usuário</h1>
-        <form onSubmit={this.cadastrar}>
-          <label>Nome :</label>
-          <input
-            type="text"
-            value={this.state.nome}
-            onChange={(e) => this.setState({ nome: e.target.value })}
-          />
-          <br/>
-          <label>Email :</label>
-          <input
-            type="email"
-            value={this.state.email}
-            onChange={(e) => this.setState({ email: e.target.value })}
-          />
-          <br/>
-          <label>Senha :</label>
-          <input
-            type="password"
-            value={this.state.senha}
-            onChange={(e) => this.setState({ senha: e.target.value })}
-          />
-          <br/>
-          <button type='submit'>Cadastrar</button>
-        </form>
+      <div className='container'>
+        <img src={biscoito} className='img'/>
+        <Botao nome='Abrir biscoito' acaoBtn={this.quebraBiscoito}/>
+        <h3 className='textofrase'>{this.state.textoFrase}</h3>
       </div>
     );
   }
 }
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+         <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
